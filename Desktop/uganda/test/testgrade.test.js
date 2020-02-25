1f8b00d8dece2517a8e5a54a64bc8911cadbc331
ibia-byte <retyibea@yahoo.com>
2020-02-25 17:18:41 +0300

    created test files

---
+++ Desktop/uganda/test/testgrade.test.js
@@ -0,0 +1,9 @@
+const mark = require("../index");
+test("test if the mark is a number", () => {
+  expect(typeof(mark.marks)).toBe("number");
+  
+
+});
+test("test if the mark is a number", () => {
+expect(mark.isValidate()).toBe("true");
+});
